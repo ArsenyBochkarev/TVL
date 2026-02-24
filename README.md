19b430c7e2c853a217ab9fd3eb79dc6d3ed8ff4f
Arseny Bochkarev <bochkarevarseny@gmail.com>
2026-02-24 15:31:16 +0300

    [TVL] Implement properties checking - Use SPIN and TLA+ - Update README & .gitignore

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # TVL
 Telecom protocols Verification Language
 
+### Prerequisites
+- Java
+- ANTLRv4
+- SBT
+- Target model checker
+  - For TLA+, make sure you have `pcal` and `tlc` set, e.g.
+    - ```
+      pcal() { java -cp <path to tla2tools.jar> -XX:+UseParallelGC -DTLA-Library= pcal.trans "$@"; }
+      export -f pcal
+      ```
+    - ```
+      tlc() { java -cp <path to tla2tools.jar> -XX:+UseParallelGC -DTLA-Library= tlc2.TLC "$@"; }
+      export -f tlc
+      ```
 
-### Building from scratch
+### Building from scratch for the first time
 ```shell
 java -jar <ANTLR .jar> -visitor -no-listener -Dlanguage=Java ./src/main/scala/Grammar/TVL.g4
 sbt compile
-translate <input file> <output file>
 ```
+
+### Usage
+```
+translate <input file> <output file> <target>
+```
+
+### Supported targets
+- TLA+ (initial translation made to PlusCal)
+- SPIN
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,42 @@
 import org.antlr.v4.runtime.*
 import Grammar.*
 import Translator.*
-import Translator.Target.Promela
+import Translator.Target.{TargetTranslator, *}
 
-def parse(input: String, output: String): Unit = {
+import java.io.{File, PrintWriter}
+
+def parse(input: String, output: String, target: String, debug: Boolean = false): Unit = {
+  if !targetIsValid(target) then
+    println(s"Error: Invalid target \"$target\". Use \"pluscal\" or \"promela\"")
+    System.exit(1)
+
   val charstream = CharStreams.fromFileName(input)
   val lexer = new TVLLexer(charstream)
   val tokens = new CommonTokenStream(lexer)
   val parser = new TVLParser(tokens)
   val tree = parser.program()
 
-  val visitor = new ASTVisitor(true)
+  val visitor = new ASTVisitor(debug)
   visitor.visitProgram(tree)
   val ir = visitor.getIR
-  val promelaTranslator = new Promela()
-  val promelaCode = promelaTranslator.translate(ir)
-
-  println(promelaCode)
+  val translator: TargetTranslator = target match {
+    case "spin" => new Promela()
+    case "tla" => new PlusCal()
+  }
+  val code = translator.translate(ir)
+  val writer = new PrintWriter(new File(output))
+  try {
+    writer.println(code)
+    writer.println(translator.getFinishingProperty)
+    writer.println(translator.getMsgDeliveredProperty)
+    writer.println(translator.getValidityProperty)
+  } finally {
+    writer.close()
+  }
 }
 
 @main
-def main(filePath: String , outputFile: String): Unit = {
-  parse(filePath, outputFile)
+def main(filePath: String, outputFile: String, target: String): Unit = {
+  parse(filePath, outputFile, target)
   System.exit(0)
 }
